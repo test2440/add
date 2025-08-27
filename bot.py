59a0fb3dfc3a52982d8ccaeace3188b98bd3d37b
test2440 <test2440t@gmail.com>
2025-08-27 16:30:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 from pyrogram import Client, idle
 from pyromod import listen
 
-OWNER_ID = int(f"1457243602")
+OWNER_ID = int(f"7291869416")
 ch = "z1_xa" 
-OWNER_USERNAME = "z1_xa"
+OWNER_USERNAME = "dev_blal"
 ST = "z1_xa"
 LT = "z1_xa"
 DEVS = []
@@ -16,8 +16,8 @@ bot_token="توكن"
 bot_token2="كود جلسه بايروجرام "
 
 
-bot = Client("ITA", api_id=8186557, api_hash="efd77b34c69c164ce158037ff5a0d117", bot_token=bot_token, plugins=dict(root="CASER"))
-lolo = Client("hossam", api_id=8186557, api_hash="efd77b34c69c164ce158037ff5a0d117", session_string=bot_token2)    
+bot = Client("ITA", api_id=9671629, api_hash="be5c84e9dc1ca0e2b53d54b71e575124", bot_token=bot_token, plugins=dict(root="CASER"))
+lolo = Client("hossam", api_id=9671629, api_hash="be5c84e9dc1ca0e2b53d54b71e575124", session_string=bot_token2)    
 
 bot_id = bot.bot_token.split(":")[0]
 
